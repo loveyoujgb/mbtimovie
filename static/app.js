4a0c27e0636a0db265d0673023a060e69473a591
DESKTOP-KBMLAIH\jsi98 <sij7304@gamil.com>
2022-07-13 23:48:27 +0900

    메인페이지 완성 (자동슬라이드 기능 구현)

--- static/app.js
+++ static/app.js
@@ -172,8 +172,33 @@ function check_dup() {
   function logout() {
             alert('로그아웃 되었습니다.')
             window.location.href = "/login"
-       $.removeCookie('mytoken', {path: '/'});
+   $.removeCookie('mytoken', {path: '/movie'});
         }
+
+//메인페이지 슬라이드 기능
+    var index = 0;   //이미지에 접근하는 인덱스
+    window.onload = function(){
+        slideShow();
+    }
+
+    function slideShow() {
+    var i;
+    var x = document.getElementsByClassName("slide1");  //slide1에 대한 dom 참조
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";   //처음에 전부 display를 none으로 한다.
+    }
+    index++;
+    if (index > x.length) {
+        index = 1;  //인덱스가 초과되면 1로 변경
+    }
+    x[index-1].style.display = "block";  //해당 인덱스는 block으로
+    setTimeout(slideShow, 3500);   //함수를 4초마다 호출
+
+}
+
+
+
+
 
 
 
